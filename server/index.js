ae0f8270e50987203a31694aba0f6d2da1e2cb47
Raja <aravindha92@gmail.com>
2023-05-15 11:58:07 +0530

    add user routes

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js"
 import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
 import { register } from "./controllers/auth.js";
 
 
@@ -46,6 +47,7 @@ app.post("/auth/register", upload.single("picture"), register);
 // Routes 
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 //MONGOOSE SETUP
 
---
+++ server/routes/users.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+    getUser,
+    getUserFriends,
+    addRemoveFriend,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend); // use path for an update function
+
+export default router;
+
