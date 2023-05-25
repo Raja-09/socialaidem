0f23a095e0f86059e227fa9e2d4ffe9f85cbc98a
Raja <aravindha92@gmail.com>
2023-05-26 00:26:31 +0530

    Set theme, start navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,31 @@
-import './App.css';
-import { BrowserRouter, Navigate, Routes, Routef, Route } from 'react-router-dom';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import HomePage from 'scenes/homePage';
 import LoginPage from 'scenes/loginPage';
 import ProfilePage from 'scenes/profilePage';
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme';
+
 
 
 function App() {
+  const mode = useSelector((state) => state.mode);//gets state 
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); //setup theme
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LoginPage />} />
-          <Route path='/home' element={<HomePage />} />
-          <Route path='/profile/:userId' element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline /> {/* reset css */}
+          <Routes>
+            <Route path='/' element={<LoginPage />} />
+            <Route path='/home' element={<HomePage />} />
+            <Route path='/profile/:userId' element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Box, IconButton, useMediaQuery, useTheme } from '@mui/material'
+
+import { useDispatch, useSelector } from 'react-redux'
+import { setMode, setLogout } from 'state'
+import { useNavigate } from 'react-router-dom'
+import FlexBetween from 'components/FlexBetween'
 
 function Navbar() {
+    const [isMobileMenuToggle, setIsMobileMenuToggled] = useState(false);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();;
+    const user = useSelector((state) => state.user);
+    const isNonMobileScreens = useMediaQuery("(min-width:100px)");
+
+    const theme = useTheme();
+    const neutralLight = theme.palette.neutral.light
+    const dark = theme.palette.neutral.dark;
+    const background = theme.palette.background.default;
+    const primaryLught = theme.palette.primary.light
+    const alt = theme.palette.background.alt;
     return (
         <div>Navbar</div>
     )
